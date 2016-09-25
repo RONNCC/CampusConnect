75e1ea7d4e8bab30564657415df9bd6838985aee
Javed Ramjohn <jramjohn@andrew.cmu.edu>
2016-09-25 17:56:39 -0400

    User Form Styling and Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ActiveRecord::Base
   has_many :skills
   
   #Validations
+  # Validations
+  validates_presence_of :last_name, :first_name, :email
+  validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"
+  validates :username, presence: true, uniqueness: { case_sensitive: false}
+  validates_presence_of :password, on: :create 
+  validates_presence_of :password_confirmation, on: :create 
+  validates_confirmation_of :password, on: :create, message: "does not match"
+  validates_length_of :password, minimum: 4, message: "must be at least 4 characters long", allow_blank: true
 
   ROLES = [['Administrator', :admin], ['Buyer', :buyer], ['Seller', :seller], ['Buyer/Seller', :bser]]
   PROLES = [['Buyer', :buyer], ['Seller', :seller], ['Buyer/Seller', :bser]]
