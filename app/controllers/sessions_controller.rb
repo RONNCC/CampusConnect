15e79eb14ad755ef24bac5628e8ceb035753a838
Javed Ramjohn <jramjohn@andrew.cmu.edu>
2016-09-26 22:45:00 -0400

    Minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       user = User.find_by_username(params[:username])
       if user && User.authenticate(params[:username], params[:password])
         session[:user_id] = user.id
-        redirect_to '/dashboard', notice: "You are logged into the CampusConnect system"
+        redirect_to '/dashboard', notice: "You are logged into the Campus Connect system"
       else
         flash[:message] = "Username and/or password is invalid"
         render "new"
